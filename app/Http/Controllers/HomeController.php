d0be423af18a620413f8f01e595f9790f3e9d59c
Evan Septya Utama <evanseptya6@gmail.com>
2023-11-01 17:11:50 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function userHome()
+    {
+        return view('home', ["msg" => "Hello! I am user"]);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function editorHome()
+    {
+        return view('home', ["msg" => "Hello! I am editor"]);
+    }
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function adminHome()
+    {
+        return view('home', ["msg" => "Hello! I am admin"]);
+    }
+}
